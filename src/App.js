e0d41590fe0dab83feb3cb197e42ba77f31e201e
kkeeth <zensin0082@gmail.com>
2020-01-10 18:47:23 +0900

    feat: all reset feature

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ const App = props => {
   const [name, setName] = useState(props.name)
   const [price, setPrice] = useState(props.price)
 
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
     <>
       <div className="App">
         <p>Now {name} is ￥{price}.</p>
 	<button onClick={() => setPrice(price + 1)}>+1</button>
 	<button onClick={() => setPrice(price - 1)}>-1</button>
-	<button onClick={() => setPrice(props.price)}>reset</button>
+	<button onClick={reset}>reset</button>
 	<input value={name} onChange={e => setName(e.target.value)} />
       </div>
     </>
