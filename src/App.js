d57ed0e2483f78df7d615eee337ce90ee9a1ef73
kkeeth <zensin0082@gmail.com>
2020-01-10 18:19:58 +0900

    fixed: revival files of lost

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      This is the Template for React App
+    </div>
+  )
+}
+
+export default App
