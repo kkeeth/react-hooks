b4b91ba0e10accfb4e2b77c1dd325c2156c9ef66
kkeeth <zensin0082@gmail.com>
2020-01-10 19:00:38 +0900

    feat: tweak

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      This is the Template for React App
-    </div>
+    <>
+      <div className="App">
+        This is the Template for React App
+      </div>
+    </>
   )
 }
 
