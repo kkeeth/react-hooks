2730d868ad81606cecaff88985b264f1bab87d5e
kkeeth <zensin0082@gmail.com>
2020-01-10 19:02:42 +0900

    feat: add components directory

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        This is the Template for React App
+      </div>
+    </>
+  )
+}
+
+export default App
