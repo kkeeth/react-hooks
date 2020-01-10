56fcdccee543cba82ee10d5578881163ff0d5690
kkeeth <zensin0082@gmail.com>
2020-01-10 21:48:30 +0900

    feat: unittest

--- src/components/UniteStates.test.js
+++ src/components/UniteStates.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import UniteStates from './UniteStates'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const { getByText } = render(<UniteStates />)
+  const linkElement = getByText(/Now/i)
   expect(linkElement).toBeTruthy()
 })
