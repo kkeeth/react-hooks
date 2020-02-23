c295276f2d4df06a02344491cc99e0bbd3b48899
kkeeth <zensin0082@gmail.com>
2020-02-23 11:33:30 +0900

    refactor: added actions and replace event title with constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { DELETE_EVENT } from '../actions'
+
 const Event = ({ event, dispatch }) => {
   const id = event.id
   const handleClickDelete = () => {
     const result = window.confirm(`Is it really good to delete the event with id = ${id} ?`)
-    if (result) dispatch({ type: 'DELETE_EVENT', id })
+    if (result) dispatch({ type: DELETE_EVENT, id })
   }
 
   return (
