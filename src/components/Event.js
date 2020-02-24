77e1feb17b39226f9e224736df95cf5c4209c1d4
kkeeth <zensin0082@gmail.com>
2020-02-24 11:42:55 +0900

    feat: implement logic of deleting operation log

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,22 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT } from '../actions'
+import { DELETE_EVENT, ADD_OPERATION_LOG } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentISO8601 } from '../utils'
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext)
   const id = event.id
   const handleClickDelete = () => {
     const result = window.confirm(`Is it really good to delete the event with id = ${id} ?`)
-    if (result) dispatch({ type: DELETE_EVENT, id })
+    if (result) {
+      dispatch({ type: DELETE_EVENT, id })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: 'deleted event',
+        operatedAt: timeCurrentISO8601(),
+      })
+    }
   }
 
   return (
